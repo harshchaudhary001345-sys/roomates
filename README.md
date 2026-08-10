533d29fb52c30fab151d697e050d545f3dd64cbf
harshchaudhary001345-sys <harshchaudhary001345@gmail.com>
2026-08-10 18:02:00 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-# @babel/generator
+# @babel/helper-compilation-targets
 
-> Turns an AST into code.
+> Helper functions on Babel compilation targets
 
-See our website [@babel/generator](https://babeljs.io/docs/babel-generator) for more information or the [issues](https://github.com/babel/babel/issues?utf8=%E2%9C%93&q=is%3Aissue+label%3A%22pkg%3A%20generator%22+is%3Aopen) associated with this package.
+See our website [@babel/helper-compilation-targets](https://babeljs.io/docs/babel-helper-compilation-targets) for more information.
 
 ## Install
 
 Using npm:
 
 ```sh
-npm install --save-dev @babel/generator
+npm install --save @babel/helper-compilation-targets
 ```
 
 or using yarn:
 
 ```sh
-yarn add @babel/generator --dev
+yarn add @babel/helper-compilation-targets
 ```
--- index.js
+++ index.js
@@ -3,106 +3,227 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = void 0;
-exports.generate = generate;
-var _sourceMap = require("./source-map.js");
-var _printer = require("./printer.js");
-function normalizeOptions(code, opts, ast) {
-  var _opts$recordAndTupleS;
-  if (opts.experimental_preserveFormat) {
-    if (typeof code !== "string") {
-      throw new Error("`experimental_preserveFormat` requires the original `code` to be passed to @babel/generator as a string");
-    }
-    if (!opts.retainLines) {
-      throw new Error("`experimental_preserveFormat` requires `retainLines` to be set to `true`");
-    }
-    if (opts.compact && opts.compact !== "auto") {
-      throw new Error("`experimental_preserveFormat` is not compatible with the `compact` option");
-    }
-    if (opts.minified) {
-      throw new Error("`experimental_preserveFormat` is not compatible with the `minified` option");
-    }
-    if (opts.jsescOption) {
-      throw new Error("`experimental_preserveFormat` is not compatible with the `jsescOption` option");
-    }
-    if (!Array.isArray(ast.tokens)) {
-      throw new Error("`experimental_preserveFormat` requires the AST to have attached the token of the input code. Make sure to enable the `tokens: true` parser option.");
+Object.defineProperty(exports, "TargetNames", {
+  enumerable: true,
+  get: function () {
+    return _options.TargetNames;
+  }
+});
+exports.default = getTargets;
+Object.defineProperty(exports, "filterItems", {
+  enumerable: true,
+  get: function () {
+    return _filterItems.default;
+  }
+});
+Object.defineProperty(exports, "getInclusionReasons", {
+  enumerable: true,
+  get: function () {
+    return _debug.getInclusionReasons;
+  }
+});
+exports.isBrowsersQueryValid = isBrowsersQueryValid;
+Object.defineProperty(exports, "isRequired", {
+  enumerable: true,
+  get: function () {
+    return _filterItems.isRequired;
+  }
+});
+Object.defineProperty(exports, "prettifyTargets", {
+  enumerable: true,
+  get: function () {
+    return _pretty.prettifyTargets;
+  }
+});
+Object.defineProperty(exports, "unreleasedLabels", {
+  enumerable: true,
+  get: function () {
+    return _targets.unreleasedLabels;
+  }
+});
+var _browserslist = require("browserslist");
+var _helperValidatorOption = require("@babel/helper-validator-option");
+var _lruCache = require("lru-cache");
+var _utils = require("./utils.js");
+var _targets = require("./targets.js");
+var _options = require("./options.js");
+var _pretty = require("./pretty.js");
+var _debug = require("./debug.js");
+var _filterItems = require("./filter-items.js");
+const browserModulesData = require("@babel/compat-data/native-modules");
+const ESM_SUPPORT = browserModulesData["es6.module"];
+const v = new _helperValidatorOption.OptionValidator("@babel/helper-compilation-targets");
+function validateTargetNames(targets) {
+  const validTargets = Object.keys(_options.TargetNames);
+  for (const target of Object.keys(targets)) {
+    if (!(target in _options.TargetNames)) {
+      throw new Error(v.formatMessage(`'${target}' is not a valid target
+- Did you mean '${(0, _helperValidatorOption.findSuggestion)(target, validTargets)}'?`));
     }
   }
-  const format = {
-    auxiliaryCommentBefore: opts.auxiliaryCommentBefore,
-    auxiliaryCommentAfter: opts.auxiliaryCommentAfter,
-    shouldPrintComment: opts.shouldPrintComment,
-    preserveFormat: opts.experimental_preserveFormat,
-    retainLines: opts.retainLines,
-    retainFunctionParens: opts.retainFunctionParens,
-    comments: opts.comments == null || opts.comments,
-    compact: opts.compact,
-    minified: opts.minified,
-    concise: opts.concise,
-    indent: {
-      adjustMultilineComment: true,
-      style: "  "
-    },
-    jsescOption: Object.assign({
-      quotes: "double",
-      wrap: true,
-      minimal: false
-    }, opts.jsescOption),
-    topicToken: opts.topicToken
-  };
-  format.decoratorsBeforeExport = opts.decoratorsBeforeExport;
-  format.jsescOption.json = opts.jsonCompatibleStrings;
-  format.recordAndTupleSyntaxType = (_opts$recordAndTupleS = opts.recordAndTupleSyntaxType) != null ? _opts$recordAndTupleS : "hash";
-  format.importAttributesKeyword = opts.importAttributesKeyword;
-  if (format.minified) {
-    format.compact = true;
-    format.shouldPrintComment = format.shouldPrintComment || (() => format.comments);
-  } else {
-    format.shouldPrintComment = format.shouldPrintComment || (value => format.comments || value.includes("@license") || value.includes("@preserve"));
-  }
-  if (format.compact === "auto") {
-    format.compact = typeof code === "string" && code.length > 500000;
-    if (format.compact) {
-      console.error("[BABEL] Note: The code generator has deoptimised the styling of " + `${opts.filename} as it exceeds the max of ${"500KB"}.`);
+  return targets;
+}
+function isBrowsersQueryValid(browsers) {
+  return typeof browsers === "string" || Array.isArray(browsers) && browsers.every(b => typeof b === "string");
+}
+function validateBrowsers(browsers) {
+  v.invariant(browsers === undefined || isBrowsersQueryValid(browsers), `'${String(browsers)}' is not a valid browserslist query`);
+  return browsers;
+}
+function getLowestVersions(browsers) {
+  return browsers.reduce((all, browser) => {
+    const [browserName, browserVersion] = browser.split(" ");
+    const target = _targets.browserNameMap[browserName];
+    if (!target) {
+      return all;
     }
+    try {
+      const splitVersion = browserVersion.split("-")[0].toLowerCase();
+      const isSplitUnreleased = (0, _utils.isUnreleasedVersion)(splitVersion, target);
+      if (!all[target]) {
+        all[target] = isSplitUnreleased ? splitVersion : (0, _utils.semverify)(splitVersion);
+        return all;
+      }
+      const version = all[target];
+      const isUnreleased = (0, _utils.isUnreleasedVersion)(version, target);
+      if (isUnreleased && isSplitUnreleased) {
+        all[target] = (0, _utils.getLowestUnreleased)(version, splitVersion, target);
+      } else if (isUnreleased) {
+        all[target] = (0, _utils.semverify)(splitVersion);
+      } else if (!isUnreleased && !isSplitUnreleased) {
+        const parsedBrowserVersion = (0, _utils.semverify)(splitVersion);
+        all[target] = (0, _utils.semverMin)(version, parsedBrowserVersion);
+      }
+    } catch (_) {}
+    return all;
+  }, {});
+}
+function outputDecimalWarning(decimalTargets) {
+  if (!decimalTargets.length) {
+    return;
   }
-  if (format.compact || format.preserveFormat) {
-    format.indent.adjustMultilineComment = false;
+  console.warn("Warning, the following targets are using a decimal version:\n");
+  decimalTargets.forEach(({
+    target,
+    value
+  }) => console.warn(`  ${target}: ${value}`));
+  console.warn(`
+We recommend using a string for minor/patch versions to avoid numbers like 6.10
+getting parsed as 6.1, which can lead to unexpected behavior.
+`);
+}
+function semverifyTarget(target, value) {
+  try {
+    return (0, _utils.semverify)(value);
+  } catch (_) {
+    throw new Error(v.formatMessage(`'${value}' is not a valid value for 'targets.${target}'.`));
   }
+}
+function nodeTargetParser(value) {
+  const parsed = value === true || value === "current" ? process.versions.node.split("-")[0] : semverifyTarget("node", value);
+  return ["node", parsed];
+}
+function defaultTargetParser(target, value) {
+  const version = (0, _utils.isUnreleasedVersion)(value, target) ? value.toLowerCase() : semverifyTarget(target, value);
+  return [target, version];
+}
+function generateTargets(inputTargets) {
+  const input = Object.assign({}, inputTargets);
+  delete input.esmodules;
+  delete input.browsers;
+  return input;
+}
+function resolveTargets(queries, env) {
+  const resolved = _browserslist(queries, {
+    mobileToDesktop: true,
+    env
+  });
+  return getLowestVersions(resolved);
+}
+const targetsCache = new _lruCache({
+  max: 64
+});
+function resolveTargetsCached(queries, env) {
+  const cacheKey = typeof queries === "string" ? queries : queries.join() + env;
+  let cached = targetsCache.get(cacheKey);
+  if (!cached) {
+    cached = resolveTargets(queries, env);
+    targetsCache.set(cacheKey, cached);
+  }
+  return Object.assign({}, cached);
+}
+function getTargets(inputTargets = {}, options = {}) {
+  var _browsers, _browsers2;
+  let {
+    browsers,
+    esmodules
+  } = inputTargets;
   const {
-    auxiliaryCommentBefore,
-    auxiliaryCommentAfter,
-    shouldPrintComment
-  } = format;
-  if (auxiliaryCommentBefore && !shouldPrintComment(auxiliaryCommentBefore)) {
-    format.auxiliaryCommentBefore = undefined;
+    configPath = ".",
+    onBrowserslistConfigFound
+  } = options;
+  validateBrowsers(browsers);
+  const input = generateTargets(inputTargets);
+  let targets = validateTargetNames(input);
+  const shouldParseBrowsers = !!browsers;
+  const hasTargets = shouldParseBrowsers || Object.keys(targets).length > 0;
+  const shouldSearchForConfig = !options.ignoreBrowserslistConfig && !hasTargets;
+  if (!browsers && shouldSearchForConfig) {
+    browsers = process.env.BROWSERSLIST;
+    if (!browsers) {
+      const configFile = options.configFile || process.env.BROWSERSLIST_CONFIG || _browserslist.findConfigFile(configPath);
+      if (configFile != null) {
+        onBrowserslistConfigFound == null || onBrowserslistConfigFound(configFile);
+        browsers = _browserslist.loadConfig({
+          config: configFile,
+          env: options.browserslistEnv
+        });
+      }
+    }
+    if (browsers == null) {
+      browsers = [];
+    }
   }
-  if (auxiliaryCommentAfter && !shouldPrintComment(auxiliaryCommentAfter)) {
-    format.auxiliaryCommentAfter = undefined;
+  if (esmodules && (esmodules !== "intersect" || !((_browsers = browsers) != null && _browsers.length))) {
+    browsers = Object.keys(ESM_SUPPORT).map(browser => `${browser} >= ${ESM_SUPPORT[browser]}`).join(", ");
+    esmodules = false;
   }
-  return format;
+  if ((_browsers2 = browsers) != null && _browsers2.length) {
+    const queryBrowsers = resolveTargetsCached(browsers, options.browserslistEnv);
+    if (esmodules === "intersect") {
+      for (const browser of Object.keys(queryBrowsers)) {
+        if (browser !== "deno" && browser !== "ie") {
+          const esmSupportVersion = ESM_SUPPORT[browser === "opera_mobile" ? "op_mob" : browser];
+          if (esmSupportVersion) {
+            const version = queryBrowsers[browser];
+            queryBrowsers[browser] = (0, _utils.getHighestUnreleased)(version, (0, _utils.semverify)(esmSupportVersion), browser);
+          } else {
+            delete queryBrowsers[browser];
+          }
+        } else {
+          delete queryBrowsers[browser];
+        }
+      }
+    }
+    targets = Object.assign(queryBrowsers, targets);
+  }
+  const result = {};
+  const decimalWarnings = [];
+  for (const target of Object.keys(targets).sort()) {
+    const value = targets[target];
+    if (typeof value === "number" && value % 1 !== 0) {
+      decimalWarnings.push({
+        target,
+        value
+      });
+    }
+    const [parsedTarget, parsedValue] = target === "node" ? nodeTargetParser(value) : defaultTargetParser(target, value);
+    if (parsedValue) {
+      result[parsedTarget] = parsedValue;
+    }
+  }
+  outputDecimalWarning(decimalWarnings);
+  return result;
 }
-exports.CodeGenerator = class CodeGenerator {
-  constructor(ast, opts = {}, code) {
-    this._ast = void 0;
-    this._format = void 0;
-    this._map = void 0;
-    this._ast = ast;
-    this._format = normalizeOptions(code, opts, ast);
-    this._map = opts.sourceMaps ? new _sourceMap.default(opts, code) : null;
-  }
-  generate() {
-    const printer = new _printer.default(this._format, this._map);
-    return printer.generate(this._ast);
-  }
-};
-function generate(ast, opts = {}, code) {
-  const format = normalizeOptions(code, opts, ast);
-  const map = opts.sourceMaps ? new _sourceMap.default(opts, code) : null;
-  const printer = new _printer.default(format, map, ast.tokens, typeof code === "string" ? code : null);
-  return printer.generate(ast);
-}
-var _default = exports.default = generate;
 
 //# sourceMappingURL=index.js.map
